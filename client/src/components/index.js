bb1b9cd309e73805fae465828c87ab9e482dead5
Adnan Husayn <42314195+Ads1710@users.noreply.github.com>
2023-09-03 12:22:59 +0100

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColourPicker from "./ColourPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { 
+  CustomButton,
+  AIPicker,
+  ColourPicker,
+  FilePicker,
+  Tab,
+};
